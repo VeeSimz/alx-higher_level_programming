cff3603f9324949b7de9c59ea00a2517b8630d18
VeeSimz <kayveesimba@gmail.com>
2024-02-13 11:47:45 -0800

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
--- Script that creates a table second_table in the database
--- hbtn_0c_0 in MySQL server and add multiple rows
+-- Script that creates a table second_table 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- Script that creates 4 records
