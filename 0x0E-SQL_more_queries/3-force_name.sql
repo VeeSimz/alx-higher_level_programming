8b813ba55cc8b7688abef8b03faee77034e082a4
VeeSimz <kayveesimba@gmail.com>
2024-02-15 18:36:50 -0800

    Force_name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
--- Create a table foce_name on MySQL server
-CREATE TABLE IF NOT EXIXTS 'force_name' (id INT, name VARCHAR(256) NOT NULL);
+-- Create a table force_name on MySQL server
+CREATE TABLE IF NOT EXISTS 'force_name' (id INT, name VARCHAR(256) NOT NULL);
