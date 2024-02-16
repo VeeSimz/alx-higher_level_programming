ca4eca8492d211cdf0633a6cf8445e1bd7f4d00e
VeeSimz <kayveesimba@gmail.com>
2024-02-16 03:43:43 +0200

    Give user select privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Create the database hbtn_0d_2 and user_0d_2
+-- User should only have SELECT privilege in the database
+-- user_0d_2 password should be set to user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
