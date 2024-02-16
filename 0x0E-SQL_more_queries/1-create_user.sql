cc92e4a2aa9d2c50fcbfea11679000ffe719c80a
VeeSimz <kayveesimba@gmail.com>
2024-02-15 18:16:13 -0800

    Fixed documentation issues

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
---Create a MySQL server user and grant all privileges
+-- Create a user user_0d_1 on MySQL server
+-- user_0d_1 should have all privileges
+-- user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
