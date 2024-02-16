6cb3dbb1aeb207ad44daa11afc67ceb5c74622e5
VeeSimz <kayveesimba@gmail.com>
2024-02-15 18:34:54 -0800

    Create read-user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
--- Create the database hbtn_0d_2 and user_0d_2
+-- Creates the database hbtn_0d_2
+-- Creates the user_0d_2
 -- User should only have SELECT privilege in the database
 -- user_0d_2 password should be set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2' .* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
