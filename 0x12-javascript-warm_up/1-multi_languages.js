f4d89e4b265bf3d2e8bb0653cd42ec7247229393
VeeSimz <kayveesimba@gmail.com>
2024-03-12 03:56:30 +0200

    fix errors

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
 const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const phrase of phrases) { console.log(phrase) };
+for (const phrase of phrases) 
+console.log(phrase);
