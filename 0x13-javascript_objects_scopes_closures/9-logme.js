9dcc1dec545aea4b538e13705256e173816b3a13
VeeSimz <kayveesimba@gmail.com>
2024-03-13 02:51:59 +0200

    Log of printed values

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (this.count === undefined) { this.count = 0; } else { this.count++; }
+  const line = this.count + ': ' + item;
+  console.log(line);
+};
